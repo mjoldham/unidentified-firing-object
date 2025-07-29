df9f31f91a5b817e55b50c98b0aa054e66c25233
xSamzz2411 <stu-348249@gapps.suffolk.ac.uk>
2025-07-30 00:39:31 +0100

    Extends Slider
    Added extends slider and made use of headers

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,19 +7,34 @@ namespace UFO
     {
         public static UIManager Instance { get; private set; }
 
+        [Header("Panels")]
         public GameObject MainMenu;
         public GameObject Credits;
         public GameObject Options;
-
         public GameObject HUD;
 
+        [Header("Extends Slider")]
+        public Slider ExtendsSlider;
+        public Text ExtendsSliderText;
+
+        [Header("Player")]
         public PlayerController Player;
 
+        [Header("HUD")]
         public Text Extends;
         public Text Bombs;
 
         private void Awake()
         {
+            // slider listeners
+            ExtendsSlider.value = Player.ExtendCount;
+            ExtendsSlider.onValueChanged.AddListener(value =>
+            {
+                Player.ExtendCount = (int) value;
+                ExtendsSliderText.text = value.ToString();
+            });
+
+            // make sure theres only 1 instance
             MainMenu.SetActive(true);
             if (Instance != null && Instance != this)
             {
